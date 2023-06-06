d07d19083597549f129858363097cdbdc6e05851
thepatochido <pato23a@gmail.com>
2023-06-05 22:31:23 -0600

    Cambios en el fondo e inicio de script de comportamiento de fondo
    Nomás agregue los assets a la carpeta del proyecto, agregué un fondo como objeto 3d para hacerlo infinito (si quieren después cambiamos la imagen) y empecé a hacerle el script pero tengo tarea de mecánica de sólidos xd

---
+++ TimeUpFI/Assets/ScrollerFondo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ScrollerFondo : MonoBehaviour
+{
+    [Range(-1f, 1f)]
+    public float velocidadScroll = 0.5f;
+    private float offset;
+    private Material material1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material1 = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
